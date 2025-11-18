3f179d8ea338fb1060414e71486fa8c77d865b8f
Frank20060 <villarredondofrank@fpllefia.com>
2025-11-18 19:04:36 +0100

    tabla cervezas

---
+++ PruebaVite2/src/components/TablaBirras.jsx
@@ -0,0 +1,29 @@
+
+
+export function TablaBirras(){
+
+    return(
+
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Birra</th>
+                        <th>Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Guinnes</td>
+                        <td>20$</td>
+                    </tr>
+                    <tr>
+                        <td>Cruzcampo</td>
+                        <td>4$</td>
+                    </tr>
+                </tbody>
+
+            </table>
+        </>
+    )
+}
